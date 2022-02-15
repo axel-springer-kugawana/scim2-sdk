bedd960e6113c22b53df01963d17424e72146f23
Benoît OYEZ <benoit.oyez-ext@groupeseloger.com>
2022-02-15 12:56:12 +0000

    Missing import

--- scim2-sdk-client/src/main/java/dev/axelspringerkugawana/scim2/client/Scim2ClientImpl.java
+++ scim2-sdk-client/src/main/java/dev/axelspringerkugawana/scim2/client/Scim2ClientImpl.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import dev.axelspringerkugawana.scim2.schema.ScimConstant;
 import dev.axelspringerkugawana.scim2.schema.data.BaseRecord;
 import dev.axelspringerkugawana.scim2.schema.data.ScimResponse;
